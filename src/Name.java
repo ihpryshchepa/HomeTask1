bd407653d6709ae8dddf1904373f5068d2152fe9
ihor <ihprishchepa@gmail.com>
2023-11-01 10:45:19 +0200

    add first task

--- src/Name.java
+++
@@ -1,6 +0,0 @@
-public class Name {
-    public static void main(String[] args) {
-        System.out.println("Number of command line arguments: " + args.length);
-        for (String name : args) System.out.println(name);
-    }
-}
---
+++ src/com/solvd/laba/block1/task1/Name.java
@@ -0,0 +1,7 @@
+package com.solvd.laba.block1.task1;
+
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("Hello! Nice to see you " + args[0] + " " + args[1]);
+    }
+}
