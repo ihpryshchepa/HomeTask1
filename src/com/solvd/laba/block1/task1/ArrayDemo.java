9e8ca024268e46eabf3890cc114fa0ded367b283
ihor <ihprishchepa@gmail.com>
2023-11-20 23:16:05 +0200

    add logger

--- src/com/solvd/laba/block1/task1/ArrayDemo.java
+++ src/com/solvd/laba/block1/task1/ArrayDemo.java
@@ -1,13 +1,23 @@
 package com.solvd.laba.block1.task1;
 
+import com.solvd.laba.block1.task2.Main;
+
 import java.util.Arrays;
 import java.util.Random;
+import java.util.logging.Logger;
+
 
 public class ArrayDemo {
+    static {
+        System.setProperty("log4j.configurationFile", "log4j2.xml");
+    }
+    private static final Logger LOGGER = Logger.getLogger("ArrayDemo");
+
     public static void main(String[] args) {
+        LOGGER.info("Hello world");
         Random rd = new Random();
         int[] array = new int[10];
-        for(int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             array[i] = rd.nextInt(100);
         }
         System.out.println("Array before sort: " + Arrays.toString(array));
